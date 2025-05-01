64c7df1aecd63e5a760328d263d7418c82bdc4b4
ivan7kov7 <ivan7kov7@gmail.com>
2025-05-01 10:37:02 +0300

    added kustomization_backup output

--- output.tf
+++ output.tf
@@ -96,6 +96,11 @@ output "kubeconfig" {
   sensitive   = true
 }
 
+output "kustomization_backup" {
+  value       = local_file.kustomization_backup.content
+  sensitive = false
+}
+
 output "kubeconfig_data" {
   description = "Structured kubeconfig data to supply to other providers"
   value       = local.kubeconfig_data
