43413c3ee12daca22b3badf7ca5e0629cdbcf13e
ivan7kov7 <ivan7kov7@gmail.com>
2025-07-21 16:21:52 +0300

    update nginx default kind to Deployment

--- locals.tf
+++ locals.tf
@@ -607,7 +607,7 @@ persistence:
   nginx_values = var.nginx_values != "" ? var.nginx_values : <<EOT
 controller:
   watchIngressWithoutClass: "true"
-  kind: "DaemonSet"
+  kind: "Deployment"
   replicaCount: ${local.ingress_replica_count}
   config:
     "use-forwarded-headers": "true"
